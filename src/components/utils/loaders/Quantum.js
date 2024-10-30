e17497e0bc3a71ce2bbc9b4ad3f496e8a928246f
GauravKesh <gkrcoder@gmail.com>
2024-10-30 12:27:55 +0530

    "Changed wrapping element from React fragment to div in Quantum component"

--- src/components/utils/loaders/Quantum.js
+++ src/components/utils/loaders/Quantum.js
@@ -4,8 +4,8 @@ import React from "react";
 export default function Quantum() {
   quantum.register();
   return (
-    <>
+    <div>
       <l-quantum size="45" speed="1.75" color="black"></l-quantum>
-    </>
+    </div>
   );
 }
